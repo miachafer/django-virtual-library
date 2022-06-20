ba52326f14ee8e592e4fe845d48130ba4e6f6528
miachafer <miachafer@gmail.com>
2022-06-20 15:58:49 -0300

    Sets up user registration and authentication

--- library/books/views.py
+++ library/books/views.py
@@ -1,11 +1,45 @@
 from django.shortcuts import render
-from django.http import request
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as do_login
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'books/home.html')
 
 def register(request):
-    return render(request, 'books/register.html')
+    if request.method == "GET":
+        return render(request, 'books/register.html')
+    else:
+        username = request.POST.get("username")
+        user_email = request.POST.get("user_email")
+        password = request.POST.get("password")
+        # The following variable searches in database to check if this user already exists.
+        user = User.objects.filter(username=username).first()
+        if user:
+            context = {
+                "message": "This user already exists."
+            }
+            return render (request, 'books/register.html', context)
+        new_user = User.objects.create_user(username=username, email=user_email, password=password)
+        new_user.save()
+        context = {
+                "message": "User registered successfully!"
+            }
+        return render (request, 'books/register.html', context)        
 
 def login(request):
-    return render(request, 'books/login.html')
+    if request.method == "GET":
+        return render(request, 'books/login.html')
+    else:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if(user):
+            do_login(request, user)
+            return HttpResponse("User authenticated successfully!")
+        else:
+            context = {
+                "message": "Wrong username or password. Try again."
+            }
+            return render(request, 'books/login.html', context)
