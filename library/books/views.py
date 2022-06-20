6682cf13db34c20deb75f7741f71c8fa1d0d62f6
miachafer <miachafer@gmail.com>
2022-06-20 15:49:43 -0300

    Sets up user registration and authentication

--- library/books/views.py
+++ library/books/views.py
@@ -1,45 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from django.contrib.auth import login as do_login
-from django.http import HttpResponse
+from django.http import request
 
 def home(request):
     return render(request, 'books/home.html')
 
 def register(request):
-    if request.method == "GET":
-        return render(request, 'books/register.html')
-    else:
-        username = request.POST.get("username")
-        user_email = request.POST.get("user_email")
-        password = request.POST.get("password")
-        # The following variable searches in database to check if this user already exists.
-        user = User.objects.filter(username=username).first()
-        if user:
-            context = {
-                "message": "This user already exists."
-            }
-            return render (request, 'books/register.html', context)
-        new_user = User.objects.create_user(username=username, email=user_email, password=password)
-        new_user.save()
-        context = {
-                "message": "User registered successfully!"
-            }
-        return render (request, 'books/register.html', context)        
+    return render(request, 'books/register.html')
 
 def login(request):
-    if request.method == "GET":
-        return render(request, 'books/login.html')
-    else:
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(username=username, password=password)
-        if(user):
-            do_login(request, user)
-            return HttpResponse("User authenticated successfully!")
-        else:
-            context = {
-                "message": "Wrong username or password. Try again."
-            }
-            return render(request, 'books/login.html', context)
+    return render(request, 'books/login.html')
