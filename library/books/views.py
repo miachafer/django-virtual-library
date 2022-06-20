813da76f71fc802c0b191fd33dc88d22cced12f1
miachafer <miachafer@gmail.com>
2022-06-20 14:10:17 -0300

    Create simple views for home, login, and register

--- library/books/views.py
+++ library/books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import request
 
-# Create your views here.
+def home(request):
+    return render(request, 'books/home.html')
+
+def register(request):
+    return render(request, 'books/register.html')
+
+def login(request):
+    return render(request, 'books/login.html')
