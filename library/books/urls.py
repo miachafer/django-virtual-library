a04f8e8847894a708219cc980c0336ac59475bb6
miachafer <miachafer@gmail.com>
2022-06-20 16:07:18 -0300

    Sets up new url to books view

--- library/books/urls.py
+++ library/books/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
+    path('books/', views.books, name='books'),
 ]
