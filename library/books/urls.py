59cc1f8133cd12d49cdc2143fde1f0768d6f1365
miachafer <miachafer@gmail.com>
2022-06-20 17:09:46 -0300

    Create new url to book registration

--- library/books/urls.py
+++ library/books/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('books/', views.books, name='books'),
+    path('books/register-a-book', views.books_register, name='books_register'),
 ]
