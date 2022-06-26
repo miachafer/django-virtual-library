43830d23465c4525365fe78cd3e57db44ae0aae6
miachafer <miachafer@gmail.com>
2022-06-26 11:25:50 -0300

    Add search field by book's name and filter by author's name

--- library/books/admin.py
+++ library/books/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    list_filter = ["author"]
+
+admin.site.register(Book, BookAdmin)
